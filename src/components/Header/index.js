e60167274aa83f847ea25d6c0440ef3b7377d61c
PebblerWon <pebblerwon@gmail.com>
2017-07-18 18:27:42 +0800

    1

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -12,30 +12,57 @@ const Header = ()=> {
   const handleClick = (e) => {
     console.log('click ', e);
   }
+  const smalColProps={
+  	xs:2,
+  	sm:4,
+  	md:3,
+  	lg:3,
+  }
+  const bigColProps={
+  	 xs:20,
+  	 sm:16,
+  	 md:18,
+  	 lg:18
+  }
     return (
     	<div className={styles.content}>
-    		
-    		<div className={styles.logo}>
-    			<img src="" alt=""/>
-    		</div>
-	      	<Menu className={styles.menu}
-		        onClick={handleClick}
-		        selectedKeys={[state.current]}
-		        mode="horizontal"
-	      	>
-		        <Menu.Item key="map" className={styles.menuItem}>
-		          <Icon type="mail" />地图浏览
-		        </Menu.Item>
-		        <Menu.Item key="data" className={styles.menuItem}>
-		          <Icon type="appstore" />数据中心
-		        </Menu.Item>
-		        <Menu.Item key="tj" className={styles.menuItem}>
-		          <Icon type="appstore" />信息统计
-		        </Menu.Item>
-		     	<Menu.Item key="userManage" className={styles.menuItem}>
-		          <Icon type="appstore" />用户管理
-		        </Menu.Item>
-	      	</Menu>
+    		<Row align="bottom" type='flex'>
+    			<Col {...smalColProps}>
+    				<h1>小河流</h1>
+    			</Col>
+    			<Col {...bigColProps}>
+    				<Menu className={styles.menu}
+				        onClick={handleClick}
+				        selectedKeys={[state.current]}
+				        mode="horizontal"
+			      	>
+				        <Menu.Item key="map" className={styles.menuItem}>
+				          <Icon type="mail" />地图浏览
+				        </Menu.Item>
+				        <Menu.Item key="data" className={styles.menuItem}>
+				          <Icon type="appstore" />数据中心
+				        </Menu.Item>
+				        <Menu.Item key="tj" className={styles.menuItem}>
+				          <Icon type="appstore" />信息统计
+				        </Menu.Item>
+				     	<Menu.Item key="userManage" className={styles.menuItem}>
+				          <Icon type="appstore" />用户管理
+				        </Menu.Item>
+			      	</Menu>
+    			</Col>
+    			<Col {...smalColProps}>
+    				<Menu mode="horizontal" style={{float: 'right',}} >
+				          <SubMenu title={<span> <Icon type="user" />
+				            admin </span>}
+				          >
+				            <Menu.Item key="logout">
+				              退出
+				            </Menu.Item>
+				          </SubMenu>
+			        </Menu>
+    			</Col>
+    		</Row>
+	      	
       	</div>
       )
 }
--- src/routes/app.js
+++ src/routes/app.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from '../components/Header'
-
-const App = ()=>{
+import styles from './app.less'
+const App = ({children})=>{
 	return (
 		<div>
 			<Header></Header>
+			<div className={styles.container}>
+					{children}
+			</div>
 		</div>
 	)
 }
